2a88432e8450ff2359872f6ea1ea553ffbadb050
julia <juliabadita@live.fr>
2022-08-20 17:55:31 +0200

    changing the favicon format, making the form responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Définit les champs du formulaire comme ''
+
+// function baseForm() {
+//   document.getElementById('name').value = ''
+//   document.getElementById('FirstName').value = ''
+//   document.getElementById('email').value = ''
+//   document.getElementById('phone').value = ''
+// }
