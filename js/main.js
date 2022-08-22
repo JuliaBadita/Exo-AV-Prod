13685c62299abaf34c783e2442368016876fc668
julia <juliabadita@live.fr>
2022-08-22 12:21:54 +0200

    pushing the "final" result of this exo

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@
 //   document.getElementById('email').value = ''
 //   document.getElementById('phone').value = ''
 // }
+
+function lettersOnlyCheck(nom) {
+  var regEx = /^[A-Za-z]+$/
+  if (nom.value.match(regEx)) {
+    return true
+  } else {
+    alert('Please enter letters only.')
+    return false
+  }
+}
